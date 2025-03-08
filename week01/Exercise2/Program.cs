352e94f225e94be95d9fd7fe20f7fecbb5fb6086
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2025-03-08 17:07:44 -0500

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("¿What is your grade percentage?");
+		Double gradePercentage = double.Parse(Console.ReadLine());
+		string gradeLetter;
+
+		if (gradePercentage >= 90)
+		{
+			gradeLetter = "A";
+		}
+		else if (gradePercentage >= 80)
+		{
+			gradeLetter = "B";
+		}
+		else if (gradePercentage >= 70)
+		{
+			gradeLetter = "C";
+		}
+		else if (gradePercentage >= 60)
+		{
+			gradeLetter = "D";
+		}
+		else
+		{
+			gradeLetter = "F";
+		}
+		
+		
+		
+		int lastdigit = Convert.ToInt32(gradePercentage) % 10;
+		String gradeSign = "";
+
+		if ((gradeLetter == "A" && lastdigit < 3) || gradeLetter!= "F")
+		{
+			if(lastdigit >= 7)
+			{
+				gradeSign = "+";
+			}
+			else if(lastdigit < 3)
+			{
+				gradeSign = "-";
+			}
+		}
+
+		Console.WriteLine($"Your grade is {gradeLetter}{gradeSign}");
+
     }
 }
--- week01/Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
+++ week01/Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ec301ae43255a33fda08c65490bd083ff5af01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553a2b6e4d73a55822414bc97c22b4c36f1d49b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
