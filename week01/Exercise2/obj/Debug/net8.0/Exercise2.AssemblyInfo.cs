553a2b6e4d73a55822414bc97c22b4c36f1d49b3
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2025-03-08 13:56:20 -0500

    fix debug error

--- week01/Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
+++ week01/Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b6aac565f52e6e4348772cf23435a86836cf175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ec301ae43255a33fda08c65490bd083ff5af01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
