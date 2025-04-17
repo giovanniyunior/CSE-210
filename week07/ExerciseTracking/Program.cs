226e4fd076c3a52d6ab9098e5d27a008e5304d1a
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2025-04-17 10:08:28 -0500

    homework week 6

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -3,15 +3,20 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.Clear();
+	{
+		Console.Clear();
 		List<Activity> activities = new List<Activity>();
 		activities.Add(new Swimming(30, 50));
 		activities.Add(new Cycling(30, 100));
 		activities.Add(new Running(30, 30));
+		Display(activities);
+	}
+
+	private static void Display(List<Activity> activities)
+	{
 		foreach (var activity in activities)
-        {
-            Console.WriteLine(activity.GetSummary());
-        }
+		{
+			Console.WriteLine(activity.GetSummary());
+		}
 	}
 }
